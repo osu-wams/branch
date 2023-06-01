efb0e2744828e2f47b92b70094d85740f88381eb
Matthew Brabham <matthew.brabham@oregonstate.edu>
2023-06-01 09:43:55 -0700

    Adding Code standards and applying them.

--- footer.php
+++ footer.php
@@ -6,56 +6,54 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package WordPress
- * @subpackage Twenty_Twenty
- * @since Twenty Twenty 1.0
+ * @package Branch
+ * @since Branch 1.0.0
  */
 
-
-
- 
 ?>
-			<footer id="site-footer" role="contentinfo" class="header-footer-group">
+<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
-				<div class="section-inner">
+	<div class="section-inner">
 
 
-					<div class="footer-credits">
-					<a href="https://oregonstate.edu/" id="osu-logo"><img src="<?php echo get_stylesheet_directory_uri() ?>/logo.svg"
-alt="Oregon State University"></a>
+		<div class="footer-credits">
+			<a href="https://oregonstate.edu/" id="osu-logo"><img
+						src="<?php echo get_stylesheet_directory_uri(); ?>/logo.svg"
+						alt="Oregon State University"></a>
 
-						<p class="footer-copyright">
-							<a href="https://oregonstate.edu/copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://www.php.net/date */
-								_x( 'Y', 'copyright date format', 'twentytwenty' )
-							);
-							?> Oregon State University</a>
-						</p><!-- .footer-copyright -->
+			<p class="footer-copyright">
+				<a href="https://oregonstate.edu/copyright">&copy;
+					<?php
+					echo date_i18n(
+					/* translators: Copyright date format, see https://www.php.net/date */
+						_x( 'Y', 'copyright date format', 'twentytwenty' )
+					);
+					?>
+					Oregon State University</a>
+			</p><!-- .footer-copyright -->
 
-					</div><!-- .footer-credits -->
+		</div><!-- .footer-credits -->
 
-					<a class="to-the-top" href="#site-header">
+		<a class="to-the-top" href="#site-header">
 						<span class="to-the-top-long">
 							<?php
 							/* translators: %s: HTML character for up arrow. */
 							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
 							?>
 						</span><!-- .to-the-top-long -->
-						<span class="to-the-top-short">
+			<span class="to-the-top-short">
 							<?php
 							/* translators: %s: HTML character for up arrow. */
 							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
 							?>
 						</span><!-- .to-the-top-short -->
-					</a><!-- .to-the-top -->
+		</a><!-- .to-the-top -->
 
-				</div><!-- .section-inner -->
+	</div><!-- .section-inner -->
 
-			</footer><!-- #site-footer -->
+</footer><!-- #site-footer -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-	</body>
+</body>
 </html>
